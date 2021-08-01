1122b196f847c2a742a80ff473316e439ea9cff9
Serhii Vartolomii <vsv21.cv@gmail.com>
2021-08-01 16:52:42 +0300

    add sass-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -67,6 +67,10 @@ module.exports = {
         }),
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.svg$/,
         use: [
           {
