2a205425605a40255c206ead020e7e2acfc7ef63
Bogdan Volkovinskiy <bogolosv@gmail.com>
2021-08-07 03:51:28 +0300

    preview files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import { Button, ButtonTheme, ButtonType, ButtonSize, ButtonForm } from "./components/atoms/Button";
+import RadioGroup from "./components/atoms/RadioGroup";
+import { useState } from "react";
+import List from "./components/atoms/List";
+import { radioMale, tableHeader, tableRows } from "./constants";
 
 function App() {
+  const [male, setMale] = useState('M');
+
+  const radioHandle = (value) => setMale(value);
+
+  const tableScroll = (listHeight,scrollPosition) => {
+    console.log(listHeight, scrollPosition);
+  }
+
   return (
     <div className="App">
-      <Button theme={ButtonTheme.DEFAULT} type={ButtonType.BUTTON} size={ButtonSize.MEDIUM}>Button</Button>
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px'}}>
+        <RadioGroup
+          label="Gender"
+          value={male}
+          items={radioMale}
+          onChange={radioHandle}
+          disabled={false}
+        />
+      </div>
+
+      <List
+        columns={tableHeader}
+        rows={tableRows}
+        onScroll={tableScroll}
+      />
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,56 @@
+export const radioMale = [
+  {
+    label: "Female",
+    value: "F"
+  },
+  {
+    label: "Male",
+    value: "M"
+  },
+  {
+    label: "Other",
+    value: "O"
+  }
+];
+
+export const tableHeader = [
+  {
+    id: '1',
+    name: '№',
+    sort: false
+  },
+  {
+    id: '2',
+    name: 'Name',
+    sort: true
+  }
+];
+
+export const tableRows = [
+  {
+    items: [
+      {
+        columnId: '1',
+        value: 1
+      },
+      {
+        columnId: '2',
+        value: 'Bogdan'
+      }
+    ],
+    id: '1'
+  },
+  {
+    items: [
+      {
+        columnId: '1',
+        value: 2
+      },
+      {
+        columnId: '2',
+        value: 'Ivan'
+      }
+    ],
+    id: '2'
+  },
+];
