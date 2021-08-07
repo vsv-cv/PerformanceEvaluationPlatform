f4f5999608d95136b218e3c3d499d1c4acf3b10a
Irynanaiko <irynaiko@ukr.net>
2021-08-07 18:06:17 +0300

    feat: add datetime component

---
+++ src/components/DateTime/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+
+const DateTime = () => {
+    return (
+        <Datetime/>
+    )
+}
+
+export default DateTime;
