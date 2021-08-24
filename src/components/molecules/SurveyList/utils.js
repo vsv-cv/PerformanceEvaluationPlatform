0677b2742591c43cd51ab3f58a850c090340e811
luckyone1104 <luckyone1104@gmail.com>
2021-08-24 11:07:13 +0300

    refactor: delete utils which are now common.

--- src/components/molecules/SurveyList/utils.js
+++ src/components/molecules/SurveyList/utils.js
@@ -1,38 +1,38 @@
-export const formatDate = date => {
-  const utcDate = new Date(date);
-  const day = ('0' + (utcDate.getUTCDate() + 1)).slice(-2);
-  const month = utcDate.getUTCMonth() + 1;
-  const year = utcDate.getUTCFullYear();
-
-  return `${day}.${month}.${year}`;
-}
+import { concatPages, formatDate } from '../../../utils';
 
 export const formatData = data => {
   if (!data) return;
 
-  const allPages =  data?.pages?.reduce((acc, page) => {
-    return acc.concat(page);
-  }, []);
+  const allPages = concatPages(data?.pages);
 
   return allPages?.map(item => ({
-    ...item, 
-    appointmentDate: formatDate(item.appointmentDate)
-  }))
+    ...item,
+    appointmentDate: formatDate(item.appointmentDate),
+  }));
 };
 
-export const getRows = rows => rows?.map(row => {
-  const items = Object
-    .entries(row)
-    .map(([key, value]) => {
-      return {
-        columnId: key,
-        value: String(value)
-      }
-    })
-    .filter(item => item.columnId !== 'id');
+export const getAssignees = data => {
+  return data?.reduce((acc, currentValue) => {
+    if (!acc.some(item => item.key === currentValue.assigneeId)) {
+      return [
+        ...acc,
+        { key: currentValue.assigneeId, text: currentValue.assignee },
+      ];
+    }
 
-  return ({
-    id: String(row.id),
-    items: items
-  })
-});
+    return acc;
+  }, []);
+};
+
+export const getSupervisors = data => {
+  return data?.reduce((acc, currentValue) => {
+    if (!acc.some(item => item.key === currentValue.supervisorId)) {
+      return [
+        ...acc,
+        { key: currentValue.supervisorId, text: currentValue.supervisor },
+      ];
+    }
+
+    return acc;
+  }, []);
+};
